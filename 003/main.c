f8b7533e754c5a96e0c01763adacafeac1f0b7b3
marceloboeira <marceloboeira@live.com>
2014-01-17 09:20:29 -0200

    Challenge 003

---
+++ 003/main.c
@@ -0,0 +1,33 @@
+/**
+ * Euler Challenge 003
+ *
+ * @see http://projecteuler.net/problem=3
+ * @see http://blog.marceloboeira.com/euler/challenge-003
+ *
+ * The prime factors of 13195 are 5, 7, 13 and 29. 
+ * What is the largest prime factor of the number 600851475143 ?
+ *
+ * Answer: 6857
+ *
+ *
+ */
+#include <stdio.h>
+
+long int pfact(long long int n) {
+    int i = 0;
+    long int temp = 0;
+    for (i = 2; i <= n; i++)  {
+        if ((n % i) == 0) {
+           temp = i;
+           n = n/ i;           
+           i = 1;
+        }    
+    }
+    return temp;
+}
+
+int main(void) {
+    pfact(600851475143);
+    return 0;
+}
+
