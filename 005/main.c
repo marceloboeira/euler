96ffb91156459769d2c4f509d8632cdc1efe15ba
marceloboeira <marceloboeira@live.com>
2014-01-17 11:30:52 -0200

    Challenges 005 and 006

---
+++ 005/main.c
@@ -0,0 +1,33 @@
+/**
+ * Euler Challenge 005
+ *
+ * @see http://projecteuler.net/problem=5
+ * @see http://blog.marceloboeira.com/euler/challenge-005
+ *
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ *
+ * Answer: 232792560
+ *
+ *
+ */
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    bool l = 0;
+    long int j = 0,i = 0;
+    while (!l) {
+       i += 20;      
+       l = 1;
+       j = 2;
+       while ((j <= 20) && (l)) {
+            l = ((i % j) == 0) && l;
+            j++;
+       }
+ 
+    }
+    printf("Answer: %i\n",i); 
+    return 0;
+}
+
---
+++ 006/main.c
@@ -0,0 +1,37 @@
+/**
+ * Euler Challenge 006
+ *
+ * @see http://projecteuler.net/problem=6
+ * @see http://blog.marceloboeira.com/euler/challenge-006
+ *
+ * The sum of the squares of the first ten natural numbers is,
+ *
+ * 1² + 2² + ... + 10² = 385
+ * The square of the sum of the first ten natural numbers is,
+ * 
+ * (1 + 2 + ... + 10)² = 552 = 3025
+ * Hence the difference between the sum of the squares 
+ * of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
+ * 
+ * Find the difference between the sum of the squares 
+ * of the first one hundred natural numbers and the square of the sum.
+ *
+ * Answer: 232792560
+ *
+ *
+ */
+#include <stdio.h>
+
+int main(void) {
+    int i = 1;
+    long int j = 0, k = 0, l = 0;
+    for (i = 1; i<= 100; i++) {
+        j += (i*i);
+        k += i;    
+    }
+    l = (k*k) - j;        
+    printf("Answer: %i\n",l); 
+    system("pause");
+    return 0;
+}
+
