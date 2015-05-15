b7979f36b4606d616fc23480f0a8c8ba99693548
Gabriel Almir <almirpereira97@gmail.com>
2015-05-14 23:15:58 -0300

    Modify the answer
    I made a mistake in my answer, the challenge says that you need to
    repeat this cycle until 1000, that is before 1000 and this doesn't
    include this number..

--- javascript/001.js
+++ javascript/001.js
@@ -5,13 +5,13 @@
 // The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 //
-// Answer: 234168
+// Answer: 233168
 
 // print(sum((n for n in range(1, 1001) if n % 3 == 0 or n % 5 == 0)))
 
 var result = 0, i;
 
-for (i = 1; i <= 1000; i++) {
+for (i = 1; i < 1000; i++) {
 	if ((i % 3 == 0) || (i % 5 == 0)) {
 		result += i;		
 	}	
