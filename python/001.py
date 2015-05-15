e4a3ba6bcf977e73bbe070d36060ab6c40c18320
Gabriel Almir <almirpereira97@gmail.com>
2015-05-14 22:59:20 -0300

    Modified the answer.

--- python/001.py
+++ python/001.py
@@ -5,6 +5,6 @@
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 #
-# Answer: 233168
+# Answer: 234168
 
 print(sum((n for n in range(1, 1001) if n % 3 == 0 or n % 5 == 0)))
