e4cfdc9487eae112e5f4da2cabf23684eaa67c28
Marcelo Boeira <me@marceloboeira.com>
2017-09-13 14:22:00 +0200

    feagure: Add haskell/001

---
+++ haskell/001.hs
@@ -0,0 +1,10 @@
+--
+-- Project Euler: Challenge 001 - http://projecteuler.net/problem=1
+--
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+-- The sum of these multiples is 23.
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+--
+-- Answer: 233168
+--
+main = do print(foldl (+) 0 (filter(\x -> mod x 3 == 0 || mod x 5 == 0) [1..1000]))
