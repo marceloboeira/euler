67b3b378e94adb90f6202e93ee2b4ed85504d8a6
Marcelo Boeira <me@marceloboeira.com>
2017-09-18 18:51:31 +0200

    feature: Add haskell/005
    It takes a while to run...

---
+++ haskell/005.hs
@@ -0,0 +1,12 @@
+--
+-- Project Euler: Challenge 005 - http://projecteuler.net/problem=5
+--
+-- 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+-- What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+--
+-- Answer: 232792560
+--
+
+divisable x = all(==True) [ mod x y == 0 | y <- [1..20] ]
+
+main = do print(take 1 [x | x <- [200000000..999999999], divisable x ])
