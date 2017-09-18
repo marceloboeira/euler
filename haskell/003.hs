af64385be9b9ee14852ef079b38774921d2f0dae
Marcelo Boeira <me@marceloboeira.com>
2017-09-18 19:05:00 +0200

    feature: Add haskell/003

---
+++ haskell/003.hs
@@ -0,0 +1,12 @@
+-- 
+-- Project Euler: Challenge 003 - http://projecteuler.net/problem=3
+-- 
+-- The prime factors of 13195 are 5, 7, 13 and 29.
+-- What is the largest prime factor of the number 600851475143 ?
+-- 
+-- Answer: 6857
+-- 
+
+prime x = all(==False) [ mod x y == 0 | y <- [2..(x-1)] ]
+
+main = do print(maximum [x | x <- [1..10000], mod 600851475143 x == 0, prime x ])
