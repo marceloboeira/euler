e200bb42adfa9ba023d0786ca10494060d830527
Marcelo Boeira <me@marceloboeira.com>
2017-09-18 22:45:40 +0200

    chore: Fix answer comment on c/006

--- c/006.c
+++ c/006.c
@@ -13,7 +13,7 @@
  * Find the difference between the sum of the squares 
  * of the first one hundred natural numbers and the square of the sum.
  *
- * Answer: 232792560
+ * Answer: 25164150
  */
 #include <stdio.h>
 
