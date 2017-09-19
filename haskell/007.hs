c7a739d5ab73f9dc43d5b64dfdabe99e278f6074
Marcelo Boeira <me@marceloboeira.com>
2017-09-19 13:57:48 +0200

    feature: Add haskell/007
    Very slow, and the check for primes is not considering 2 as a prime
    number because of the way is implemented.
    
    Soon I look forward to create a module abstraction for it

---
+++ haskell/007.hs
@@ -0,0 +1,14 @@
+--
+-- Project Euler: Challenge 007 - http://projecteuler.net/problem=7
+--
+-- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,
+-- we can see that the 6th prime is 13.
+-- What is the 10 001st prime number?
+--
+-- Answer: 104743
+--
+
+prime x = all(==False) [ mod x y == 0 | y <- [2..(x-1)] ]
+
+-- 10002 because the prime function checks from 2 and so on...
+main = do print(last (take 10002 [x | x <- [1..20000000], prime x ]))
